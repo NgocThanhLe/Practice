397f97d1065a557fc694481e0dc46b55943ec6f3
ngoc_thanh <nhokthanhpro2000@gmail.com>
2022-11-03 15:32:14 -0400

    hello

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
 "use strict";
-
-const dropdownButtonEl = document.querySelector(".dropdown_button");
-const dropdownMenuEl = document.querySelector(".dropdown_menu");
-console.log("");
-
-dropdownButtonEl.addEventListener("click", function () {
-  dropdownMenuEl.classList.toggle("is-active");
-});
